155cb62308fd5276414120eff9f6c87f2182b16f
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-20 13:23:53 -0600

    Section 2 - Blazor Files Overview

--- TangyWeb_Server/Program.cs
+++ TangyWeb_Server/Program.cs
@@ -18,6 +18,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
 
 app.UseHttpsRedirection();
 
