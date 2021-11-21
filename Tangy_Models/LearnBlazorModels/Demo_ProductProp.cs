7fd54ca48cc1b4a54967d32d8e748d53aa596443
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-21 12:30:59 -0600

    Section 3 - Dropdowns and Data Binding

---
+++ Tangy_Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models.LearnBlazorModels
+{
+    public class Demo_ProductProp
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
