3d59aac4624c6b1aeca4c3c6e047dee12fed006d
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-23 11:34:51 -0600

    Section 3 - Product List

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -12,6 +12,6 @@ namespace Tangy_Models.LearnBlazorModels
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
-        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+        public List<Demo_ProductProp> ProductProperties { get; set; }
     }
 }
