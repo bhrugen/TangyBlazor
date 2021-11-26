bcb742e4fdd8cd83d2be57bca72a869a8cc71553
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-26 14:28:26 -0600

    Section 6 - Assignment 7 Solution - SweetAlert

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -6,3 +6,21 @@
         toastr.error(message, "Operation Failed", { timeOut: 5000 });
     }
 }
+
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
+    }
+}
