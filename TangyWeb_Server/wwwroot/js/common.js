96b008f5f17a8f39ca0cddf6f76ba26c3e1730fb
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-26 13:39:15 -0600

    Section 6 - Toastr JS

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,8 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    }
+    if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
