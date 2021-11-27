ba58d36e3289c1ac095e0889696baf9f87708cae
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-27 16:00:50 -0600

    Section 8 -  ICategory Repository

---
+++ Tangy_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO);
+        public CategoryDTO Update(CategoryDTO objDTO);
+        public int Delete(int id);
+        public CategoryDTO Get(int id);
+        public IEnumerable<CategoryDTO> GetAll();
+    }
+}
