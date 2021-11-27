909a0abcd49bcf84f76c2f913e760d39a46c9c8b
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-27 16:07:21 -0600

    Section 8 - Category Repository Setup

---
+++ Tangy_Business/Repository/CategoryRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy_Business.Repository.IRepository;
+using Tangy_DataAccess;
+using Tangy_DataAccess.Data;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public CategoryDTO Create(CategoryDTO objDTO)
+        {
+            Category category = new Category()
+            {
+                Name = objDTO.Name,
+                Id = objDTO.Id,
+                CreatedDate=DateTime.Now
+            };
+
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+
+            return new CategoryDTO()
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoryDTO Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CategoryDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CategoryDTO Update(CategoryDTO objDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
