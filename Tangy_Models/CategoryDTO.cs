ab0adca97e7964b94d6df40183afd03f2a7f09d6
Bhrugen Patel <bhrugen1990@gmail.com>
2021-11-28 11:25:45 -0600

    Section 9 - List Category

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Tangy_Models
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Please enter name..")]
         public string Name { get; set; }
     }
 }
