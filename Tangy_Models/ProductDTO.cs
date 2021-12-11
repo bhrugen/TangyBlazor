f59d844bb629d5eaebdf3d194c4c1fe457c41cc2
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-11 11:51:31 -0600

    Section 11 - Product DTO

---
+++ Tangy_Models/ProductDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Desciption { get; set; }
+        public bool ShopFavorites { get; set; }
+        public bool CustomerFavorites { get; set; }
+        public string Color { get; set; }
+        public string ImageUrl { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category")]
+        public int CategoryId { get; set; }
+        
+        public CategoryDTO Category { get; set; }
+    }
+}
