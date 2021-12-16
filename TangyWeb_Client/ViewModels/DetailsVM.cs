1165e671b503687928472cbbfefa00e400003030
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-16 17:38:40 -0600

    Section 14 - Details VM

---
+++ TangyWeb_Client/ViewModels/DetailsVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tangy_Models;
+
+namespace TangyWeb_Client.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            ProductPrice = new();
+            Count = 1;
+        }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value greater than 0")]
+        public int Count { get; set; }
+        [Required]
+        public int SelectedProductPriceId { get; set; }
+        public ProductPriceDTO ProductPrice { get; set; }
+    }
+}
