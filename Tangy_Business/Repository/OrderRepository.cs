b662cb79a511c7e601c76c264874f832ee9a705c
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-17 16:46:37 -0600

    Section 16 - Order Create Method

---
+++ Tangy_Business/Repository/OrderRepository.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy_Business.Repository.IRepository;
+using Tangy_DataAccess;
+using Tangy_DataAccess.Data;
+using Tangy_DataAccess.ViewModel;
+using Tangy_Models;
+
+namespace Tangy_Business.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public OrderRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        public async Task<OrderDTO> Create(OrderDTO objDTO)
+        {
+            try
+            {
+                var obj = _mapper.Map<OrderDTO, Order>(objDTO);
+                _db.OrderHeaders.Add(obj.OrderHeader);
+                await _db.SaveChangesAsync();
+
+                foreach (var details in obj.OrderDetails)
+                {
+                    details.OrderHeaderId = obj.OrderHeader.Id;
+                }
+                _db.OrderDetails.AddRange(obj.OrderDetails);
+                await _db.SaveChangesAsync();
+
+                return new OrderDTO()
+                {
+                    OrderHeader = _mapper.Map<OrderHeader, OrderHeaderDTO>(obj.OrderHeader),
+                    OrderDetails = _mapper.Map<IEnumerable<OrderDetail>, IEnumerable<OrderDetailDTO>>(obj.OrderDetails).ToList()
+                };
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return objDTO;
+        }
+
+        public Task<int> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderDTO> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<OrderDTO>> GetAll(string? userId = null, string? status = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderHeaderDTO> MarkPaymentSuccessful(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OrderHeaderDTO> UpdateHeader(OrderHeaderDTO objDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateOrderStatus(int orderId, string status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -17,5 +17,7 @@ namespace Tangy_DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductPrice> ProductPrices { get; set; }
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
     }
 }
