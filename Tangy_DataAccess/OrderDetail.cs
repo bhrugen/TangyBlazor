0a4d46ebf978c4ca160b379e48dc556cafec78d5
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-17 16:17:33 -0600

    Section 16 - Create Order Header and Detail

---
+++ Tangy_DataAccess/OrderDetail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_DataAccess
+{
+    public class OrderDetail
+    {
+        public int Id { get; set; }
+        [Required]
+        public int OrderHeaderId { get; set; }
+
+        [Required]
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [NotMapped]
+        public virtual Product Product { get; set; }
+
+        [Required]
+        public int Count { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string Size { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+
+
+    }
+}
---
+++ Tangy_DataAccess/OrderHeader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_DataAccess
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        // add nagivation property : #TODO
+
+        [Required]
+        [Display(Name = "Order Total")]
+        public double OrderTotal { get; set; }
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+        [Required]
+        [Display(Name = "Shipping Date")]
+        public DateTime ShippingDate { get; set; }
+        [Required]
+        public string Status { get; set; }
+
+        //stripe payment
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+
+        [Display(Name = "Name")]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        [Display(Name = "Postal Code")]
+        public string PostalCode { get; set; }
+
+    }
+}
