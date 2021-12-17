25479b738f99b962bc5901e59fdbc52be0284a5a
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-17 17:09:56 -0600

    Section 16 - API - Create order controller

---
+++ TangyWeb_API/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Tangy_Business.Repository.IRepository;
+using Tangy_Models;
+
+namespace TangyWeb_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository;
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _orderRepository.GetAll());
+        }
+
+        [HttpGet("{orderHeaderId}")]
+        public async Task<IActionResult> Get(int? orderHeaderId)
+        {
+            if (orderHeaderId==null || orderHeaderId==0)
+            {
+                return BadRequest(new ErrorModelDTO()
+                {
+                    ErrorMessage="Invalid Id",
+                    StatusCode=StatusCodes.Status400BadRequest
+                });
+            }
+
+            var orderHeader = await _orderRepository.Get(orderHeaderId.Value);
+            if (orderHeader==null)
+            {
+                return BadRequest(new ErrorModelDTO()
+                {
+                    ErrorMessage="Invalid Id",
+                    StatusCode=StatusCodes.Status404NotFound
+                });
+            }
+
+            return Ok(orderHeader);
+        }
+    }
+}
