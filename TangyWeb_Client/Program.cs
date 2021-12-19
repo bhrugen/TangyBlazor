313c213cf6fefb6299d0139ec661f8c7e6c34aa8
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-19 17:27:25 -0600

    Section 19 - Custom Authentication State Demo

--- TangyWeb_Client/Program.cs
+++ TangyWeb_Client/Program.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TangyWeb_Client;
@@ -14,4 +15,6 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IOrderService,OrderService>();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider,AuthStateProvider>();
 await builder.Build().RunAsync();
