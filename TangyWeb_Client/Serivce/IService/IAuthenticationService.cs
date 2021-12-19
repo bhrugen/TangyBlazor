2660f2761888a3f0c32f0c5d6bd0cefa70c369c7
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-19 17:32:00 -0600

    Section 19 - IAuthenticationSerivce

---
+++ TangyWeb_Client/Serivce/IService/IAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using Tangy_Models;
+
+namespace TangyWeb_Client.Serivce.IService
+{
+    public interface IAuthenticationService
+    {
+        Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequestDTO);
+        Task<SignInResponseDTO> Login(SignInRequestDTO signInRequestDTO);
+        Task Logout();
+    }
+}
