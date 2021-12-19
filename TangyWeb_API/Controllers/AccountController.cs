cb4f3f0eaa4942d24c990125a72d6c8ca7f041f2
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-19 15:55:39 -0600

    Section 18 - Sign In Endpoint Part 2

--- TangyWeb_API/Controllers/AccountController.cs
+++ TangyWeb_API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using Tangy_Common;
@@ -95,7 +96,30 @@ namespace TangyWeb_API.Controllers
                 }
 
                 //everything is valid and we need to login 
-                
+                var signinCredentials = GetSigningCredentials();
+                var claims = await GetClaims(user);
+
+                var tokenOptions = new JwtSecurityToken(
+                    issuer: _aPISettings.ValidIssuer,
+                    audience: _aPISettings.ValidAudience,
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(30),
+                    signingCredentials: signinCredentials);
+
+                var token = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+                return Ok(new SignInResponseDTO()
+                {
+                    IsAuthSuccessful=true,
+                    Token = token,
+                    UserDTO = new UserDTO()
+                    {
+                        Name = user.Name,
+                        Id = user.Id,
+                        Email = user.Email,
+                        PhoneNumber = user.PhoneNumber
+                    }
+                });
 
             }
             else
@@ -115,7 +139,7 @@ namespace TangyWeb_API.Controllers
         {
             var secret = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_aPISettings.SecretKey));
 
-            return new SigningCredentials(secret,SecurityAlgorithms.Sha256);
+            return new SigningCredentials(secret,SecurityAlgorithms.HmacSha256);
         }
 
         private async Task<List<Claim>> GetClaims(ApplicationUser user)
