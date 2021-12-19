282f8d1db847aaa70fa78bb6c31c0f2a84fd611a
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-19 13:28:20 -0600

    Section 18 - SignUp API Endpoint

--- TangyWeb_API/Controllers/AccountController.cs
+++ TangyWeb_API/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Tangy_Common;
+using Tangy_DataAccess;
+using Tangy_Models;
 
 namespace TangyWeb_API.Controllers
 {
@@ -21,9 +24,45 @@ namespace TangyWeb_API.Controllers
             _signInManager = signInManager;
             _roleManager=roleManager;
         }
-        public IActionResult Index()
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp([FromBody] SignUpRequestDTO signUpRequestDTO)
         {
-            return View();
+            if(signUpRequestDTO==null || !ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var user = new ApplicationUser
+            {
+                UserName = signUpRequestDTO.Email,
+                Email = signUpRequestDTO.Email,
+                Name = signUpRequestDTO.Name,
+                PhoneNumber = signUpRequestDTO.PhoneNumber,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user,signUpRequestDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(new SignUpResponseDTO()
+                {
+                    IsRegisterationSuccessful=false,
+                    Errors= result.Errors.Select(u => u.Description)
+                });
+            }
+
+            var roleResult = await _userManager.AddToRoleAsync(user, SD.Role_Customer);
+            if (!roleResult.Succeeded)
+            {
+                return BadRequest(new SignUpResponseDTO()
+                {
+                    IsRegisterationSuccessful=false,
+                    Errors= result.Errors.Select(u => u.Description)
+                });
+            }
+            return StatusCode(201);
         }
     }
 }
