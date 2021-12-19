bcf7446f001a84bf55c26658554eea4c81921213
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-19 12:59:39 -0600

    Section 18 - Account Controller

---
+++ TangyWeb_API/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TangyWeb_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager=roleManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
