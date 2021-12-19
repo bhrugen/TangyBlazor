57fa2b4faf084619366546fbe517a3a4213c2533
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-19 17:43:01 -0600

    Section 19 - Login Service

---
+++ TangyWeb_Client/Serivce/AuthenticationService.cs
@@ -0,0 +1,56 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+using Tangy_Common;
+using Tangy_Models;
+using TangyWeb_Client.Serivce.IService;
+
+namespace TangyWeb_Client.Serivce
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly HttpClient _client;
+        private readonly ILocalStorageService _localStorage;
+        private readonly AuthenticationStateProvider _authStateProvider;
+
+        public AuthenticationService(HttpClient client, ILocalStorageService localStorage, AuthenticationStateProvider authStateProvider)
+        {
+            _client = client;
+            _authStateProvider = authStateProvider;
+            _localStorage = localStorage;
+        }
+
+        public async Task<SignInResponseDTO> Login(SignInRequestDTO signInRequest)
+        {
+            var content = JsonConvert.SerializeObject(signInRequest);
+            var bodyContent = new StringContent(content, Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync("api/account/signin", bodyContent);
+            var contentTemp = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<SignInResponseDTO>(contentTemp);
+
+            if (response.IsSuccessStatusCode)
+            {
+                await _localStorage.SetItemAsync(SD.Local_Token, result.Token);
+                await _localStorage.SetItemAsync(SD.Local_UserDetails, result.UserDTO);
+                _client.DefaultRequestHeaders.Authorization= new AuthenticationHeaderValue("bearer", result.Token);
+                return new SignInResponseDTO() { IsAuthSuccessful = true };
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Tangy_Common/SD.cs
+++ Tangy_Common/SD.cs
@@ -20,5 +20,6 @@ namespace Tangy_Common
         public const string Role_Customer = "Customer";
 
         public const string Local_Token = "JWT Token";
+        public const string Local_UserDetails = "UserDetails";
     }
 }
