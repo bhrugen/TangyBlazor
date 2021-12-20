bf5158f779850487fdc2821d87e11c7add559141
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-20 14:42:49 -0600

    Section 19 - Login in Action

---
+++ TangyWeb_Client/Pages/Authentication/Login.razor.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using Tangy_Models;
+using TangyWeb_Client.Serivce.IService;
+
+namespace TangyWeb_Client.Pages.Authentication
+{
+    public partial class Login
+    {
+        private SignInRequestDTO SignInRequest = new();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowSignInErrors { get; set; }
+        public string Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService _authSerivce { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        private async Task LoginUser()
+        {
+            ShowSignInErrors=false;
+            IsProcessing=true;
+            var result = await _authSerivce.Login(SignInRequest);
+            if (result.IsAuthSuccessful)
+            {
+                //regiration is successful
+                _navigationManager.NavigateTo("/");
+            }
+            else
+            {
+                //failure
+                Errors=result.ErrorMessage;
+                ShowSignInErrors=true;
+
+            }
+            IsProcessing=false;
+        }
+    }
+}
