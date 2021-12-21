47a1d18c392e9cac367e7903fbaee9ab396335da
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-20 22:50:04 -0600

    Section 20 - Call Stripe

---
+++ TangyWeb_Client/wwwroot/js/StripePayment.js
@@ -0,0 +1,4 @@
+﻿redirectToCheckout = function (sessionId) {
+    var stripe = Stripe("pk_test_51JcFLuLzMgCIgSRrMw19EzJALAjYC6j5mooETO1VrDWRziVUKPLyBS5nY4siaaxnTgxWg69QZIQNpUBl06WECbzB00gG4hqVX5");
+    stripe.redirectToCheckout({ sessionId: sessionId });
+}
