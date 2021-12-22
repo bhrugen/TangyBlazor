086b6c5ae68abbcc499564ba3334eaeb1bbb6637
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-21 21:23:44 -0600

    Section 21 - Update Order Details

--- Tangy_Business/Repository/OrderRepository.cs
+++ Tangy_Business/Repository/OrderRepository.cs
@@ -125,10 +125,19 @@ namespace Tangy_Business.Repository
         {
             if (objDTO!=null)
             {
-                var OrderHeader = _mapper.Map<OrderHeaderDTO, OrderHeader>(objDTO);
-                _db.OrderHeaders.Update(OrderHeader);
+                var orderHeaderFromDb = _db.OrderHeaders.FirstOrDefault(u => u.Id==objDTO.Id);
+                orderHeaderFromDb.Name = objDTO.Name;
+                orderHeaderFromDb.PhoneNumber = objDTO.PhoneNumber;
+                orderHeaderFromDb.Carrier=objDTO.Carrier;
+                orderHeaderFromDb.Tracking= objDTO.Tracking;
+                orderHeaderFromDb.StreetAddress=objDTO.StreetAddress;
+                orderHeaderFromDb.City=objDTO.City;
+                orderHeaderFromDb.State=objDTO.State;
+                orderHeaderFromDb.PostalCode=objDTO.PostalCode;
+                orderHeaderFromDb.Status=objDTO.Status;
+
                 await _db.SaveChangesAsync();
-                return _mapper.Map<OrderHeader, OrderHeaderDTO>(OrderHeader);
+                return _mapper.Map<OrderHeader, OrderHeaderDTO>(orderHeaderFromDb);
             }
             return new OrderHeaderDTO();
         }
