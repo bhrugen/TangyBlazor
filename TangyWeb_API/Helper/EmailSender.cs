06aa9ac96c604870b9b618b761f7cb4f9d81ed0e
Bhrugen Patel <bhrugen1990@gmail.com>
2021-12-22 12:09:34 -0600

    Section 22 - Send Email using SendGrid

--- TangyWeb_API/Helper/EmailSender.cs
+++ TangyWeb_API/Helper/EmailSender.cs
@@ -1,27 +1,41 @@
 ﻿using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using MimeKit;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace TangyWeb_API.Helper
 {
     public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
             try
             {
-                var emailToSend = new MimeMessage();
-                emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
-                emailToSend.To.Add(MailboxAddress.Parse(email));
-                emailToSend.Subject = subject;
-                emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+                var client = new SendGridClient(SendGridSecret);
+                var from = new EmailAddress("hello@dotnetmastery.com", "Tangy");
+                var to = new EmailAddress(email);
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+                await client.SendEmailAsync(msg);
+                //var emailToSend = new MimeMessage();
+                //emailToSend.From.Add(MailboxAddress.Parse("hello@dotnetmastery.com"));
+                //emailToSend.To.Add(MailboxAddress.Parse(email));
+                //emailToSend.Subject = subject;
+                //emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
 
-                //send email
-                using var emailClient = new SmtpClient();
-                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
-                emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$*");
-                emailClient.Send(emailToSend);
-                emailClient.Disconnect(true);
+                ////send email
+                //using var emailClient = new SmtpClient();
+                //emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                //emailClient.Authenticate("dotnetmastery@gmail.com", "DotNet123$*");
+                //emailClient.Send(emailToSend);
+                //emailClient.Disconnect(true);
             }
             catch (Exception ex)
             {
